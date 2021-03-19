076869f305961ec8605c84c7f1d43846257e79d1
Ahmad Tariq <75003816+ahmadbintariq4u@users.noreply.github.com>
2021-03-19 11:36:54 +0500

    Dealing With Layouts...

--- app/src/main/java/com/comsats/lifelines/practice.java
+++ app/src/main/java/com/comsats/lifelines/practice.java
@@ -1,19 +1,37 @@
 package com.comsats.lifelines;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.solver.state.State;
 
+import android.graphics.Path;
 import android.os.Bundle;
-
-import com.comsats.lifelines.databinding.ActivityPracticeBinding;
+import android.view.View;
+import com.comsats.lifelines.databinding.ActivityPracticeFocusBinding;
 
 public class practice extends AppCompatActivity {
 
-    ActivityPracticeBinding binding;
+    ActivityPracticeFocusBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding=ActivityPracticeBinding.inflate(getLayoutInflater());
+        binding=ActivityPracticeFocusBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        binding.focus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                binding.button1.requestFocus(View.FOCUS_DOWN);
+            }
+        });
+
+        binding.editTextTextPersonName.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                binding.editTextTextPersonName.setText("Focused state Changed..");
+            }
+        });
+
+
     }
 
 
