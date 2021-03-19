bae6de87652c24b9bb07ccb36b7c1dfbb85a042b
Ahmad Tariq <75003816+ahmadbintariq4u@users.noreply.github.com>
2021-03-19 12:17:05 +0500

    Dealing With FABs..

--- app/src/main/java/com/comsats/lifelines/MainActivity.java
+++ app/src/main/java/com/comsats/lifelines/MainActivity.java
@@ -3,12 +3,33 @@ package com.comsats.lifelines;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.comsats.lifelines.databinding.ActivityMainBinding;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(R.layout.activity_main);
+
+        binding.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(binding.getRoot(), "Here's a Snackbar", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+
+                Log.i("ToBe","This is clicked");
+
+            }
+        });
+
     }
 }
