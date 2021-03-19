3c0eea5d5e3cde057c49b526853c57da1d829465
Ahmad Tariq <75003816+ahmadbintariq4u@users.noreply.github.com>
2021-03-19 12:28:48 +0500

    Stucked in Spinner

--- app/src/main/java/com/comsats/lifelines/MainActivity.java
+++ app/src/main/java/com/comsats/lifelines/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 
 import com.comsats.lifelines.databinding.ActivityMainBinding;
 import com.google.android.material.snackbar.Snackbar;
@@ -20,16 +21,30 @@ public class MainActivity extends AppCompatActivity {
         binding=ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(R.layout.activity_main);
 
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.planets_array, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        binding.spinner.setAdapter(adapter);
+
+
+
+
+
+
+
         binding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Snackbar.make(binding.getRoot(), "Here's a Snackbar", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
-
                 Log.i("ToBe","This is clicked");
-
             }
         });
+
+
+
 
     }
 }
